0fb063a478091bdf6bb248ecb5d11291b965bfcf
Omeed <omeed.rabbani@gmail.com>
2021-09-20 01:01:40 -0700

    Update WiFi-Garden-Hub.ino

--- WiFi-Garden-Hub.ino
+++ WiFi-Garden-Hub.ino
@@ -1,4 +1,5 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
 #define LED_PIN D4
 #define SENSOR_PIN A0
@@ -6,12 +7,20 @@
 const char* ssid = "ESPHub";
 const char* password = "esphub_sensor";
 
-WiFiServer server(80);
+ESP8266WebServer server(80);
+
+void handleRoot();
+void handleCalibrateGet();
+void handleCalibratePost();
+void handleSensorPost();
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  delay(50);
+  
   boolean result = false;
   Serial.print("Setting up AP");
+  WiFi.mode(WIFI_AP);
   while(!result) {
     Serial.print(".");
     result = WiFi.softAP(ssid, password);
@@ -21,12 +30,13 @@ void setup() {
   Serial.print("AP IP address: ");
   Serial.println(ip);
 
-  server.on("/", HTTP_GET, serveRoot);
+  server.on("/", HTTP_GET, handleRoot);
   server.on("/calibrate", HTTP_GET, handleCalibrateGet);
   server.on("/calibrate", HTTP_POST, handleCalibratePost);
   server.on("/sensor", HTTP_POST, handleSensorPost);
 
   server.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop() {
@@ -46,6 +56,8 @@ void handleCalibratePost() {
 }
 
 void handleSensorPost() {
+  Serial.print("Args: ");
+  Serial.println(server.args());
   if(!server.hasArg("data") || !server.hasArg("id") || server.arg("data")==NULL || server.arg("id")==NULL) {
     server.send(400, "text/plain", "400: Invalid Request");
   } else { // success
